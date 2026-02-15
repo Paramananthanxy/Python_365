b6262b81307df83105c7faf5eb86db8ccb1d3b33
paramananthanxy <paramananthannatesan79@gmail.com>
2026-02-15 13:56:09 +0530

    Initial commit

---
+++ Scripts/Try_it_Yourself_3.py
@@ -0,0 +1,25 @@
+#Guest list 
+
+Guest = ['alen', 'randy', 'domnic', 'roman','alisha']
+print("Hi",Guest[0],"You are invited to the party ")
+
+Not_availabe = Guest.remove('domnic')
+print(Not_availabe , "unable to attend the party today" )
+Guest.insert(2,'Rey')
+print("so that",Guest[2],"Will attend the party with us..")
+
+Guest.append('Rambo')
+
+#shrinking Guest list:
+print('You can invite only two people for the dinner .')
+
+crash = Guest.pop()
+print("Sorry",crash,"Unfortunately we can't invite to the dinner because, they dinner table is not arrived..")
+crash = Guest.pop()
+print("Sorry",crash,"Unfortunately we can't invite to the dinner because, they dinner table is not arrived..")
+crash = Guest.pop()
+print("Sorry",crash,"Unfortunately we can't invite to the dinner because, they dinner table is not arrived..")
+
+print(Guest)
+# del Guest[2]
+
---
+++ Scripts/tryityouself1.py
@@ -0,0 +1,11 @@
+#store person name in a variable.
+name = "Erick"
+print ("Hello ", name)
+
+
+#strip 
+name = " Randy orton"
+name = name.title().strip()
+print(name)
+
+import this
